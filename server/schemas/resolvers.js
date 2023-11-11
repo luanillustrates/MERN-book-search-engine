9cf8bd45f12e9f390c53127605b1ad75914bf00e
luanillustrates <luanillustrates@gmail.com>
2023-11-11 23:38:49 +1030

    refactor to match criteria

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,63 +1,47 @@
 const { User } = require('../models');
-const { signToken, AuthenticationError } = require('../utils/auth');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    me: async (parent, args, context) => {
-      if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id })
-          .select('-__v -password')
-          .populate('savedBooks');
-
-        return userData;
-      }
-      throw AuthenticationError;
+    user: async (parent, { userId }) => {
+      const data = await User.findOne({ _id: userId }).select('-__v -password');
+      return data;
     },
   },
+
   Mutation: {
+    saveBook: async (parent, { userId, bookInfo }) => {
+      const data = await User.findOneAndUpdate(
+        { _id: userId },
+        { $push: { savedBooks: bookInfo } },
+        { new: true }
+      );
+      return data;
+    },
+    deleteBook: async (parent, { userId, bookId }) => {
+      const data = await User.findOneAndUpdate(
+        { _id: userId },
+        { $pull: { savedBooks: { bookId } } },
+        { new: true }
+      );
+      return data;
+    },
+    createUser: async (parent, argu) => {
+      const data = await User.create(argu);
+      const token = signToken(data);
+      return { token, data };
+    },
     login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        throw AuthenticationError;
+      const data = await User.findOne({ email });
+      if (!data) {
+        return;
       }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw AuthenticationError;
+      const passCheck = data.isCorrectPassword(password);
+      if (!passCheck) {
+        return;
       }
-      const token = signToken(user);
-
-      return { token, user };
-    },
-    addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
-      const token = signToken(user);
-
-      return { token, user };
-    },
-    saveBook: async (parent, { input }, context) => {
-      if (context.user) {
-        const updatedUser = await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $addToSet: { savedBooks: input } },
-          { new: true }
-        ).populate('savedBooks');
-        return updatedUser;
-      }
-      throw AuthenticationError;
-    },
-    removeBook: async (parent, { bookId }, context) => {
-      if (context.user) {
-        const updatedUser = await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $pull: { savedBooks: { bookId: bookId } } },
-          { new: true }
-        ).populate('savedBooks');
-        return updatedUser;
-      }
-      throw AuthenticationError;
+      const token = signToken(data);
+      return { token, data };
     },
   },
 };
--- server/server.js
+++ server/server.js
@@ -1,15 +1,14 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-
 const { ApolloServer } = require('@apollo/server');
-const { authMiddleware } = require('./utils/auth');
 const { expressMiddleware } = require('@apollo/server/express4');
+const path = require('path');
+const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 const server = new ApolloServer({
   typeDefs,
@@ -21,11 +20,11 @@ const server = new ApolloServer({
 const startApolloServer = async () => {
   await server.start();
 
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
   app.use('/graphql', expressMiddleware(server));
-  // if we're in production, serve client/build as static assets
+
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
     app.get('*', (req, res) => {
@@ -35,8 +34,8 @@ const startApolloServer = async () => {
 
   db.once('open', () => {
     app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+      console.log(`Server up at http://localhost:${PORT}`);
+      console.log(`GraphQL up at http://localhost:${PORT}/graphql`);
     });
   });
 };
